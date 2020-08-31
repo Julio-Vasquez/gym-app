01233a338b1ee2c91841ff52255bf414c5670260
Julio-Vasquez <jualvalitube@gmail.com>
2020-08-31 14:54:02 -0500

    init app

---
+++ public/electron.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron')
+
+const { join } = require('path');
+const isDev = require('electron-is-dev');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({ width: 900, height: 680 });
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${join(__dirname, '../build/index.html')}`);
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+  mainWindow.webContents.openDevTools();
+  mainWindow.on('closed', () => mainWindow = null);
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
