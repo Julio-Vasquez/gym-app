0283ba4c94c5e9fca083f356f37a2093ff417bc8
Julio-Vasquez <jualvalitube@gmail.com>
2020-09-19 18:17:57 -0500

    feat(routes): new routes

--- src/views/App.js
+++ src/views/App.js
@@ -1,17 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import Private from './Private/Private';
-import Public from './Public/Public';
+import Private from "./Private/Private";
+import Public from "./Public/Public";
 
 function App() {
-
   const auth = false;
 
-  return (
-    <div className="App">
-      {auth ? <Private /> : <Public />}
-    </div>
-  );
+  return <div className="App">{auth ? <Private /> : <Public />}</div>;
 }
 
 export default App;
--- src/views/Public/Public.js
+++ src/views/Public/Public.js
@@ -1,21 +1,35 @@
-import React, { lazy, Suspense } from 'react'
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import React, { lazy, Suspense } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
-import { Loading } from './../../components/Loading';
+import { Loading } from "./../../components/Loading";
 
-const Login = lazy(() => import('./Login'));
+const Login = lazy(() => import("./Login"));
+const ResetPassword = lazy(() => import("./ResetPassword"));
 
 const Public = () => {
   return (
     <Router>
       <Suspense fallback={<Loading />}>
         <Switch>
-          <Route exact path="/" component={Login} />
+          <Route exact path="/">
+            <Login appName="Cool Gym" />
+          </Route>
+          <Route exact path="/login">
+            <Login appName="Cool Gym" />
+          </Route>
+          <Route exact path="/reset-password">
+            <ResetPassword appName="Cool Gym" />
+          </Route>
           <Redirect from="*" to="/" />
         </Switch>
       </Suspense>
     </Router>
-  )
-}
+  );
+};
 
-export default Public
+export default Public;
