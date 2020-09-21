d048976d2e42aeeadfaf9cd239446bb9781e1822
Julio-Vasquez <jualvalitube@gmail.com>
2020-09-21 15:35:18 -0500

    feat(auth): Functional login

--- src/views/Public/Login/Login.js
+++ src/views/Public/Login/Login.js
@@ -1,14 +1,26 @@
 import React, { useState } from "react";
+import { Form, Input } from "antd";
+import { UserOutlined, AlipayOutlined } from "@ant-design/icons";
+import { useDispatch } from "react-redux";
 import PropTypes from "prop-types";
+
+import { auth } from "../../../services/Auth/actions";
 
 const Login = ({ appName }) => {
   const name = appName.split(" ");
   const [form, setForm] = useState({ user: "", password: "" });
+  const dispatch = useDispatch();
 
   const onChangeForm = (e) => {
     console.log([e.target.name] + ":" + [e.target.value]);
     setForm({ ...form, [e.target.name]: [e.target.value] });
   };
+
+  const onFinishForm = (e) => {
+    dispatch(auth.login(form.user, form.password));
+  };
+
+  const { Item } = Form;
 
   return (
     <div className="fragmento">
@@ -22,33 +34,59 @@ const Login = ({ appName }) => {
         </div>
       </div>
       <br />
-      <div className="login">
-        <input
-          type="text"
-          placeholder="username"
-          name="user"
-          onChange={onChangeForm}
-        />
-        <br />
-        <input
-          type="password"
-          placeholder="password"
-          name="password"
-          onChange={onChangeForm}
-        />
-        <br />
+      <Form onChange={onChangeForm} onFinish={onFinishForm}>
+        <div className="login">
+          <Item
+            name="usernameI"
+            rules={[
+              {
+                required: true,
+                message: "Nombre de usuario es requerido",
+              },
+              { min: 4, message: "Mínimo debe haber 4 caracteres" },
+              { max: 45, message: "Máximo debe haber  45 caracteres" },
+            ]}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              type="text"
+              placeholder="Nombre de usuario"
+              name="username"
+            />
+          </Item>
+          <Item
+            name="passwordI"
+            rules={[
+              {
+                required: true,
+                message: "Contraseña es requerida",
+              },
+              { min: 4, message: "Mínimo debe haber 4 caracteres" },
+              { max: 45, message: "Máximo debe haber  45 caracteres" },
+            ]}
+          >
+            <Input
+              prefix={<AlipayOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="********"
+              name="password"
+            />
+          </Item>
 
-        <input type="button" value="Login" />
-        <br />
-        <br />
-        <a href="/reset-password" id="forgot">
-          Olvidaste tu contraseña?
-        </a>
-      </div>
+          <input type="button" value="Login" />
+          <br />
+          <br />
+          <a href="/reset-password" id="forgot">
+            Olvidaste tu contraseña?
+          </a>
+        </div>
+      </Form>
     </div>
   );
 };
+
 Login.propTypes = {
   appName: PropTypes.string.isRequired,
 };
+
 export default Login;
