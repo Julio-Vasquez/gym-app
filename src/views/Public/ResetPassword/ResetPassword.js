12c72f88ac36bd14b0914c8bf679a1da9c251ee7
Julio Vasquez <jualvalitube@gmail.com>
2020-09-24 13:02:47 -0500

    feat(style):floating button

--- src/views/Public/ResetPassword/ResetPassword.js
+++ src/views/Public/ResetPassword/ResetPassword.js
@@ -34,64 +34,72 @@ const ResetPassword = ({ appName }) => {
   return loading ? (
     <Loading />
   ) : (
-    <div className="bg-reset">
-      <Col
-        xs={{ span: 22, offset: 1 }}
-        sm={{ span: 18, offset: 3 }}
-        lg={{ span: 14, offset: 5 }}
-        xl={{ span: 12, offset: 6 }}
-        xxl={{ span: 10, offset: 7 }}
-        className="forgot"
-      >
-        <Card className="forgot-password">
-          <Row justify="center">
-            <Col span={lock ? 24 : 18} className="forgot-password__R">
-              <h2 className="text-center">¿Olvidó su contraseña?</h2>
-              <p className="text-center">
-                Aquí puedes restablecer tu contraseña
+      <div className="bg-reset">
+        <Col
+          xs={{ span: 22, offset: 1 }}
+          sm={{ span: 18, offset: 3 }}
+          lg={{ span: 14, offset: 5 }}
+          xl={{ span: 12, offset: 6 }}
+          xxl={{ span: 10, offset: 7 }}
+          className="forgot"
+        >
+          <Card className="forgot-password">
+            <Row justify="center">
+              <Col span={lock ? 24 : 18} className="forgot-password__R">
+                <h2 className="text-center">¿Olvidó su contraseña?</h2>
+                <p className="text-center">
+                  Aquí puedes restablecer tu contraseña
               </p>
-              <Form onChange={onChange} onFinish={onSubmit}>
-                <Item
-                  name="username"
-                  rules={[
-                    {
-                      required: true,
-                      message: "Por favor ingresa su nombre de usuario",
-                    },
-                    {
-                      min: 4,
-                      message:
-                        "El nombre de usuario debe contener al menos 4 caracteres",
-                    },
-                    {
-                      max: 45,
-                      message:
-                        "El nombre de usuario debe contener maximo 45 caracteres",
-                    },
-                  ]}
-                >
-                  <Input
-                    prefix={<UserOutlined className="site-form-item-icon" />}
-                    placeholder="Username"
-                  />
-                </Item>
-                <div className="forgot-password__R__btn">
-                  <Button type="primary" htmlType="submit">
-                    Restaurar contraseña
+                <Form onChange={onChange} onFinish={onSubmit}>
+                  <Item
+                    name="username"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Por favor ingresa su nombre de usuario",
+                      },
+                      {
+                        min: 4,
+                        message:
+                          "El nombre de usuario debe contener al menos 4 caracteres",
+                      },
+                      {
+                        max: 45,
+                        message:
+                          "El nombre de usuario debe contener maximo 45 caracteres",
+                      },
+                    ]}
+                  >
+                    <Input
+                      prefix={<UserOutlined className="site-form-item-icon" />}
+                      placeholder="Username"
+                    />
+                  </Item>
+                  <div className="forgot-password__R__btn">
+                    <Button type="primary" htmlType="submit">
+                      Restaurar contraseña
                   </Button>
-                </div>
-              </Form>
-            </Col>
-          </Row>
-        </Card>
-        <div id="loading"></div>
-        <div className="reset-password-footer">
-          &copy;{name[0]}
-          <span> {name[1]} </span>
-        </div>
-      </Col>
-    </div>
-  );
+                  </div>
+                </Form>
+              </Col>
+            </Row>
+          </Card>
+          <div className="reset-password-footer">
+            &copy;{name[0]}
+            <span> {name[1]} </span>
+          </div>
+        </Col>
+
+        <a href="/login" className="made-with-mk">
+          <div class="brand">
+            <img src={require('./../../../assets/img/house.png')} alt="Icon Home" />
+          </div>
+          <div class="made-with-text">
+            Volver al<span> Login</span>
+          </div>
+        </a>
+      </div>
+    );
 };
 
 ResetPassword.propTypes = {
