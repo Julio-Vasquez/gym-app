e0690457717fe1b6ccaa744798cfe2b875d1dfac
Julio-Vasquez <jualvalitube@gmail.com>
2020-09-27 16:41:32 -0500

    feat(design): check user

--- src/views/Private/Check/Check.js
+++ src/views/Private/Check/Check.js
@@ -1,8 +1,12 @@
-import React, { useState } from 'react'
-import { ModalCheck } from './components/ModalCheck'
+import React, { useState } from "react";
+import { Form, Input, Button, Col, Card } from "antd";
+import { IdcardOutlined } from "@ant-design/icons";
+import { ModalCheck } from "./components/ModalCheck";
 
 const Check = () => {
-  const [open, setOpen] = useState(true);
+  const { Item } = Form;
+  const [identification, setIdentification] = useState();
+  const [open, setOpen] = useState(false);
   const [ok, setOk] = useState(false);
 
   const closeModal = () => setOpen(!open);
@@ -10,12 +14,69 @@ const Check = () => {
   const okModal = () => {
     setOpen(false);
     setOk(true);
-  }
+  };
 
-  const data = [{ name: 'julio', lastName: 'vasquez' }];
+  const onChnageForm = (e) => {
+    console.log(e.target.value)
+    setIdentification(e.target.value);
+  };
 
+  const onFinishForm = (e) => {
+    console.log(`Finish Form Whit ID : ${identification}`)
+  };
+
+  const data = [{ name: "julio", lastName: "vasquez" }];
   return (
-    <div>
+    <div className="check">
+      <Col
+        xs={{ span: 22, offset: 1 }}
+        sm={{ span: 18, offset: 3 }}
+        lg={{ span: 14, offset: 5 }}
+        xl={{ span: 12, offset: 6 }}
+        xxl={{ span: 10, offset: 7 }}
+        className="check-content"
+      >
+        <Card className="check-content-card">
+          <h2 className="text-center">Control de ingreso</h2>
+          <p className="text-center">
+            Aquí debe ingresar su número de documento para verificar su estado
+            de afiliación
+          </p>
+          <Form onChange={onChnageForm} onFinish={onFinishForm}>
+            <Item
+              name="identification"
+              rules={[
+                {
+                  required: true,
+                  message: "Número de identificación requerido",
+                },
+                {
+                  min: 6,
+                  message:
+                    "El número identificación debe contener mínimo 6 caracteres",
+                },
+                {
+                  max: 14,
+                  message:
+                    "El número identificación debe contener máximo 14 caracteres",
+                },
+              ]}
+            >
+              <Input
+                prefix={<IdcardOutlined className="site-form-item-icon" />}
+                placeholder="Número de identificación"
+                type="number"
+              />
+            </Item>
+            <div className="check-content-card-btn">
+              <Button type="primary" htmlType="submit">
+                Ingresar
+              </Button>
+            </div>
+          </Form>
+        </Card>
+      </Col>
+
       <ModalCheck
         data={data}
         open={open}
@@ -24,9 +85,8 @@ const Check = () => {
         close={closeModal}
         accept={ok}
       />
-      <p>checkeo</p>
     </div>
-  )
-}
+  );
+};
 
-export default Check
+export default Check;
