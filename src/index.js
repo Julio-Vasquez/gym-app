77fdab3fd377956bd5a52eb04c2bd10e5321adc3
Julio Vasquez <jualvalitube@gmail.com>
2020-09-29 16:37:39 -0500

    style(refactor)

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
-
 import { store, history } from "./store";
 
 import App from "./views/App";
 import "./sass/main.scss";
 
 import * as serviceWorker from "./serviceWorker";
-
-
 
 ReactDOM.render(
   <Provider store={store}>
