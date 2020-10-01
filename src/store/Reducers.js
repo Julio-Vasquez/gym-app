5baae0801a7a79b0be3e227e08988250c5889241
Julio Vasquez <jualvalitube@gmail.com>
2020-10-01 04:49:51 -0500

    feat(store): update imports

--- src/store/Reducers.js
+++ src/store/Reducers.js
@@ -6,24 +6,31 @@ import { auth } from "../services/Auth/AuthActions";
 import reducerAuth, {
   INITIAL_STATE as initialAuth,
 } from "./../services/Auth/AuthReducer";
+
 import reducerCheck, {
   INITIAL_STATE as initialCheck,
 } from './../services/Check/CheckReducer';
 
-const appReducers = (history) =>
-  combineReducers({
+import reducerClients, {
+  INITIAL_STATE as initialClients
+} from "../services/Clients/ClientsReducer";
+
+const appReducers = (history) => combineReducers(
+  {
     router: connectRouter(history),
     Auth: reducerAuth,
-    Check: reducerCheck
-  });
+    Check: reducerCheck,
+    Clients: reducerClients
+  }
+);
 
-console.log(auth.logout().type);
 
 export const rootReducers = (history) => (state, action) => {
   if (action.type === "AUTH/LOGOUT" || action.type === auth.logout().type)
     state = {
       Auth: initialAuth,
-      Check: initialCheck
+      Check: initialCheck,
+      Clients: initialClients
     };
   return appReducers(history)(state, action);
 };
--- src/store/Sagas.js
+++ src/store/Sagas.js
@@ -1,7 +1,13 @@
 import { fork, all } from "redux-saga/effects";
 
 import AuthSaga from "../services/Auth/AuthSaga";
+import ClientsSaga from '../services/Clients/ClientsSaga';
 
 export default function* rootSaga() {
-  yield all([fork(AuthSaga)]);
+  yield all(
+    [
+      fork(AuthSaga),
+      fork(ClientsSaga)
+    ]
+  );
 }
