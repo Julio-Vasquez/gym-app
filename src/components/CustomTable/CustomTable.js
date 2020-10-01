ab1e0efbb2157101fd611158aada73215c98a33a
Julio Vasquez <jualvalitube@gmail.com>
2020-10-01 04:45:39 -0500

    feat(table): new table

--- src/components/CustomTable/CustomTable.js
+++ src/components/CustomTable/CustomTable.js
@@ -1,17 +1,92 @@
-import React from 'react';
-import { Table } from 'antd';
+import React, { useState } from 'react';
+import { Table, Input, Button } from 'antd';
+import { SearchOutlined } from "@ant-design/icons";
+import Highlighter from "react-highlight-words";
 import PropTypes from 'prop-types';
 
-import { Search } from '../Search';
-
 export const CustomTable = ({ data, Columns, title }) => {
+  let searchInput;
+  const [searchText, setSearchText] = useState("");
+  const [searchedColumn, setSearchedColumn] = useState("");
+
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText("");
+  };
+
+  const getColumnSearchProps = (dataIndex, title) => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={
+            (node) => searchInput = node
+          }
+          placeholder={`Buscar por ${title}`}
+          value={selectedKeys[0]}
+          onChange={
+            e => setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={
+            () => handleSearch(selectedKeys, confirm, dataIndex)
+          }
+          style={{ width: 188, marginBottom: 8, display: "block" }}
+        />
+        <Button
+          type="primary"
+          onClick={
+            () => handleSearch(selectedKeys, confirm, dataIndex)
+          }
+          size="small"
+          style={{ width: 90, marginRight: 8 }}
+        >
+          <SearchOutlined />
+          Buscar
+        </Button>
+        <Button
+          onClick={
+            () => handleReset(clearFilters)
+          }
+          size="small"
+          style={{ width: 90 }}
+        >
+          Limpiar
+        </Button>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined style={{ color: filtered ? "#1890ff" : undefined }} />
+    ),
+    onFilter: (value, record) => record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownVisibleChange: (visible) => {
+      if (visible) setTimeout(() => searchInput.select());
+    },
+    render: (text) => searchedColumn === dataIndex
+      ? <Highlighter
+        highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
+        searchWords={[searchText]}
+        autoEscape
+        textToHighlight={text.toString()}
+      />
+      : (text)
+  });
+
   return (
     <div className="table">
       <div className="table__title">
         <h2>{title}</h2>
       </div>
       <div className="table__body">
-        <Table columns={Columns(Search)} dataSource={data} />
+        <Table
+          columns={Columns(getColumnSearchProps)}
+          dataSource={data}
+          rowKey="name"
+        />
       </div>
     </div>
   )
