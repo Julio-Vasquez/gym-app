34291538d0f81c70da17f58ac47e51bb0f834068
Julio Vasquez <jualvalitube@gmail.com>
2020-10-01 21:11:30 -0500

    style(import): import chechSaga

--- src/store/Sagas.js
+++ src/store/Sagas.js
@@ -2,12 +2,14 @@ import { fork, all } from "redux-saga/effects";
 
 import AuthSaga from "../services/Auth/AuthSaga";
 import ClientsSaga from '../services/Clients/ClientsSaga';
+import CheckSaga from '../services/Check/CheckSaga';
 
 export default function* rootSaga() {
   yield all(
     [
       fork(AuthSaga),
-      fork(ClientsSaga)
+      fork(ClientsSaga),
+      fork(CheckSaga)
     ]
   );
 }
