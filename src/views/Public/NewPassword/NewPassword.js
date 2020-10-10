70000212d4345775c7ae8f8732357470a556380b
Julio Vasquez <jualvalitube@gmail.com>
2020-10-10 01:26:33 -0500

    feat(set-password): view of new password

---
+++ src/views/Public/NewPassword/NewPassword.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import { Token } from './../../../common/storage'
+import { ErrorToken } from './components/ErrorToken';
+import { SetPassword } from './components/SetPassword';
+
+export default function NewPassword() {
+  const { token } = useParams();
+  
+  return Token.CheckToken(token) 
+  ? ( <SetPassword token={token} /> ) 
+  : ( <ErrorToken /> );
+}
---
+++ src/views/Public/NewPassword/components/ErrorToken/ErrorToken.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const ErrorToken = () => (
+  <div id="notfound">
+  <div className="notfound">
+    <div className="notfound-404">
+      <h1>404</h1>
+      <h2>Page not found</h2>
+    </div>
+    <a href="/">Inicio</a>
+  </div>
+</div>
+);
