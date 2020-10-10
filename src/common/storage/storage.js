cf0fb308e9c074fad05348f675afc39a40b1fb7a
Julio Vasquez <jualvalitube@gmail.com>
2020-10-10 01:22:54 -0500

    feat(storage): new method

--- src/common/storage/storage.js
+++ src/common/storage/storage.js
@@ -1,3 +1,5 @@
+import jwtDecode from "jwt-decode";
+
 class token {
   IsTokenValid = () =>
     this.GetToken() !== undefined &&
@@ -13,6 +15,18 @@ class token {
   };
 
   RemoveToken = () => sessionStorage.removeItem("coolgym");
+
+  CheckToken = (token) => {
+    try {
+      const currentToken = jwtDecode(token);
+      return (
+        currentToken !== undefined &&
+        currentToken.exp >= Math.round(new Date().getTime() / 1000)
+      );
+    } catch (e) {
+      return false;
+    }
+  };
 }
 
 export const Token = new token();
