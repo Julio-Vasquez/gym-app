58a2f8a55cf57684a18ae85ddd512b07ae48fd96
Julio Vasquez <jualvalitube@gmail.com>
2020-10-10 01:23:40 -0500

    feat(new password): actions, reducer and saga

--- src/services/Auth/AuthActions.js
+++ src/services/Auth/AuthActions.js
@@ -11,5 +11,9 @@ export const { auth } = createActions({
     RESET_PASSWORD: (userName) => ({ userName }),
     RESET_PASSWORD_SUCCESS: (success) => ({ success }),
     RESET_PASSWORD_FAILED: (error) => ({ error }),
+
+    NEW_PASSWORD: (pwd, token) => ({ pwd, token }),
+    NEW_PASSWORD_SUCCESS:(success) => ({ success }),
+    NEW_PASSWORD_FAILED: (error) => ({ error })
   },
 });
--- src/services/Auth/AuthSaga.js
+++ src/services/Auth/AuthSaga.js
@@ -50,10 +50,30 @@ function* FetchForgotPassword({ payload }) {
   }
 }
 
+function* FetchNewPassword({ payload }) {
+  try {
+    const res = yield Api.PUT("auth/forgot-password", payload);
+    if (res && res.payload.success) {
+      yield put(auth.newPasswordSuccess("ok"));
+      History.push("/login");
+    } else if (res.payload.error) {
+      message.error(`${res.payload.detail}`, 5);
+      yield put(auth.newPasswordFailed(`${res.payload.detail}`));
+    } else {
+      message.error(`Error Desconocido`);
+      const err = new TypeError("ERROR_RESET_PASSWORD");
+      yield put(auth.newPasswordFailed(err));
+    }
+  } catch (e) {
+    yield put(auth.newPasswordFailed(FailedConnectionServer()));
+  }
+}
+
 function* ActionWatcher() {
   yield takeLatest(auth.login, FetchLogin);
   yield takeLatest(auth.resetPassword, FetchForgotPassword);
   yield takeLatest(auth.logout, FetchLogout);
+  yield takeLatest(auth.newPassword, FetchNewPassword)
 }
 
 export default function* AuthSaga() {
