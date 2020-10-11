5b779428266fc8865bfa11a531c2611a6ded4e93
Julio Vasquez <jualvalitube@gmail.com>
2020-10-11 01:36:21 -0500

    feat(view): create trainer  with a modal

--- src/views/Private/Trainers/Trainer.js
+++ src/views/Private/Trainers/Trainer.js
@@ -1,26 +1,47 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { message } from "antd";
 
-import { Loading } from './../../../components/Loading';
-import { CustomTable } from '../../../components/CustomTable';
-import { Columns } from './components/Columns';
+import { Loading } from "./../../../components/Loading";
+import { CustomTable } from "../../../components/CustomTable";
+import { Columns } from "./components/Columns";
+import { FloatingButton } from "../../../components/FloatingButton";
+import { ModalNewClient } from "../../../components/ModalNewClient";
 
-import { clients } from './../../../services/Clients/ClientsActions';
+import { clients } from "./../../../services/Clients/ClientsActions";
 
 const Trainer = () => {
+  const [openModalNewTrainer, setOpenModalNewTrainer] = useState(false);
   const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(clients.getClients('Entrenador'));
-  }, [dispatch])
+  const { listClients, loading, successCreate } = useSelector(
+    (state) => state.Clients
+  );
 
-  const { listClients, loading } = useSelector(state => state.Clients);
-  return loading
-    ? <Loading />
-    : <CustomTable
-      title="Entrenadores"
-      data={listClients}
-      Columns={Columns}
-    />
-}
+  useEffect(() => {
+    dispatch(clients.getClients("Entrenador"));
+  }, [dispatch, successCreate]);
+
+  useEffect(() => {
+    if (successCreate) message.success("Entrenador creado");
+  }, [successCreate]);
+
+  return loading ? (
+    <Loading />
+  ) : (
+    <div>
+      <CustomTable title="Entrenadores" data={listClients} Columns={Columns} />
+      <FloatingButton
+        title="Agregar Nuevo Entrenador"
+        state={openModalNewTrainer}
+        setState={setOpenModalNewTrainer}
+      />
+      <ModalNewClient
+        open={openModalNewTrainer}
+        close={setOpenModalNewTrainer}
+        title="Registrar nuevo Entrenador"
+      />
+    </div>
+  );
+};
 
 export default Trainer;
