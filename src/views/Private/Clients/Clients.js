afaf3510a3f0546d9dbab97f5c9f34c108524784
Julio Vasquez <jualvalitube@gmail.com>
2020-10-11 01:35:35 -0500

    feat(view): create client with a modal

--- src/views/Private/Clients/Clients.js
+++ src/views/Private/Clients/Clients.js
@@ -1,27 +1,48 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import { Loading } from './../../../components/Loading';
-import { CustomTable } from '../../../components/CustomTable';
-import { Columns } from './components/Columns';
+import { Loading } from "./../../../components/Loading";
+import { CustomTable } from "../../../components/CustomTable";
+import { Columns } from "./components/Columns";
+import { ModalNewClient } from "../../../components/ModalNewClient";
+import { FloatingButton } from "../../../components/FloatingButton";
 
-import { clients } from './../../../services/Clients/ClientsActions';
+import { clients } from "./../../../services/Clients/ClientsActions";
+import { message } from "antd";
 
 const Clients = () => {
+  const [openModalNewClient, setOpenModalNewClient] = useState(false);
   const dispatch = useDispatch();
+
+  const { listClients, loading, successCreate } = useSelector(
+    (state) => state.Clients
+  );
+
   useEffect(() => {
-    dispatch(clients.getClients('Cliente'));
-  }, [dispatch])
+    dispatch(clients.getClients("Cliente"));
+  }, [dispatch, successCreate]);
 
-  const { listClients, loading } = useSelector(state => state.Clients);
+  useEffect(() => {
+    if (successCreate) message.success("cliente creado");
+  }, [successCreate]);
 
-  return loading
-    ? <Loading />
-    : <CustomTable
-      data={listClients}
-      title="Mis Clientes"
-      Columns={Columns}
-    />
-}
+  return loading ? (
+    <Loading />
+  ) : (
+    <>
+      <CustomTable data={listClients} title="Mis Clientes" Columns={Columns} />
+      <FloatingButton
+        title="Agregar nuevo cliente"
+        state={openModalNewClient}
+        setState={setOpenModalNewClient}
+      />
+      <ModalNewClient
+        open={openModalNewClient}
+        close={setOpenModalNewClient}
+        title="Registrar Nuevo Cliente"
+      />
+    </>
+  );
+};
 
 export default Clients;
