51912c9446203b2c0129496f1d1178ed777ae409
Julio Vasquez <jualvalitube@gmail.com>
2020-10-15 23:33:04 -0500

    feat(service): update cliente

--- src/services/Clients/ClientsSaga.js
+++ src/services/Clients/ClientsSaga.js
@@ -30,8 +30,7 @@ function* FetchCreateClient({ payload }) {
     console.log(res);
     if (res && res.payload.success) {
       yield put(clients.createClientSuccess(res.payload.payload));
-      //message.success("cliente creado");
-      //window.location.reload();
+      message.success("cliente creado");
     } else if (res.payload.error) {
       message.error(`${res.payload.detail}`);
       yield put(clients.createClientFailed(`${res.payload.detail}`));
@@ -46,7 +45,30 @@ function* FetchCreateClient({ payload }) {
   }
 }
 
-function* FetchUpdateClient(identification, newCLient) {}
+function* FetchUpdateClient({ payload }) {
+  console.log(payload);
+  try {
+    const res = yield Api.PUT("users/update", {
+      oldId: payload.identification,
+      ...payload.newClient,
+    });
+    console.log(res);
+    if (res && res.payload.success) {
+      yield put(clients.updateClientSuccess(res.payload.success));
+      message.success(res.payload.detail);
+    } else if (res.payload.error) {
+      message.error(`${res.payload.detail}`);
+      yield put(clients.updateClientFailed(`${res.payload.detail}`));
+    } else {
+      message.error(`Error Desconocido`);
+      yield put(
+        clients.updateClientFailed(new TypeError("ERROR_CHECK_PEOPLE"))
+      );
+    }
+  } catch (e) {
+    yield put(clients.createClientFailed(FailedConnectionServer()));
+  }
+}
 
 function* ActionWatcher() {
   yield takeLatest(clients.getClients, FetchGetClients);
