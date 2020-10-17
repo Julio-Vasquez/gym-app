fa072b6d9b6fb891ebad590cb954ae60fcdf37c3
Julio Vasquez <jualvalitube@gmail.com>
2020-10-16 21:53:00 -0500

    feat(component): Addtime

---
+++ src/components/AddTime/AddTime.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { Button, Space, Modal, Input, Form, Row } from "antd";
+import PropTypes from "prop-types";
+import { useDispatch } from "react-redux";
+
+import { suscription } from "./../../services/Suscription/SuscriptionActions";
+
+export const AddTime = ({ identification, open, close }) => {
+  const { Item } = Form;
+
+  const [form, setForm] = useState({
+    identification: identification,
+    cost: 0,
+    days: 0,
+  });
+
+  const dispatch = useDispatch();
+
+  const onChangeForm = (e) => {
+    console.log(e.target.name + ":" + e.target.value);
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const onFinishForm = (e) => {
+    console.log(form);
+    dispatch(suscription.addSuscription(form));
+  };
+
+  return (
+    <Space>
+      <Modal
+        centered={true}
+        title="Agregar Tiempo"
+        visible={open}
+        onCancel={() => close(false)}
+        footer={[<div key="btn-modal"></div>]}
+      >
+        <Form onChange={onChangeForm} onFinish={onFinishForm}>
+          <Row>
+            <Item
+              name="days"
+              label="Dias"
+              rules={[
+                {
+                  required: true,
+                  message: "Cantidad de dias requerido",
+                },
+                {
+                  min: 1,
+                  message: "Minimo 1 dia",
+                },
+              ]}
+            >
+              <Input name="days" type="number" />
+            </Item>
+          </Row>
+          <Row>
+            <Item
+              name="costI"
+              label="Precio"
+              rules={[
+                { required: true, message: "Costo Requerido" },
+                {
+                  min: 4,
+                  message: "El costo minimo es 1000",
+                },
+              ]}
+            >
+              <Input name="cost" type="number" />
+            </Item>
+          </Row>
+          <Row>
+            <Button type="primary" htmlType="submit">
+              Agregar Tiempo
+            </Button>
+          </Row>
+        </Form>
+      </Modal>
+    </Space>
+  );
+};
+
+AddTime.propTypes = {
+  identification: PropTypes.any.isRequired,
+  open: PropTypes.bool.isRequired,
+  close: PropTypes.func.isRequired,
+};
---
+++ src/components/AddTime/index.js
@@ -0,0 +1,1 @@
+export { AddTime } from "./AddTime";
