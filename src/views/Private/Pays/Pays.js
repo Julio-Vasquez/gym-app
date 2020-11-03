3fda8f2f389d4871ef53ddc74202da981ea4b819
Julio-Vasquez <jualvalitube@gmail.com>
2020-11-03 00:56:52 -0500

    feat(detail): details payments user specify

--- src/views/Private/Pays/Pays.js
+++ src/views/Private/Pays/Pays.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { Fragment, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Radio, Button, DatePicker, Row, InputNumber } from "antd";
 
 import { report } from "./../../../services/Report/ReportActions";
+import { PaymentsUser } from "../../../components/PaymentsUser";
 
 const Pays = () => {
+  const { success, user } = useSelector((state) => state.Report);
   const { Group } = Radio;
   const { RangePicker } = DatePicker;
 
@@ -35,33 +37,61 @@ const Pays = () => {
   };
 
   return (
-    <Row>
-      <Group onChange={onChangeRadio} value={type}>
-        <Radio value="identification">Identificación</Radio>
-        <Radio value="dates">Fechas</Radio>
-      </Group>
-      {type === "dates" ? (
-        <RangePicker format="YYYY-MM-DD" onChange={onChangeRange} />
-      ) : (
-        <InputNumber
-          max={999999999999}
-          onChange={onChangeInput}
-          required
-          onKeyUp={checkId}
-          placeholder="Número Documento"
-          style={{ width: "160px" }}
-        />
+    <Fragment>
+      <Row>
+        <Group onChange={onChangeRadio} value={type}>
+          <Radio value="identification">Identificación</Radio>
+          <Radio value="dates">Fechas</Radio>
+        </Group>
+        {type === "dates" ? (
+          <RangePicker format="YYYY-MM-DD" onChange={onChangeRange} />
+        ) : (
+          <InputNumber
+            max={999999999999}
+            onChange={onChangeInput}
+            required
+            onKeyUp={checkId}
+            placeholder="Número Documento"
+            style={{ width: "160px" }}
+          />
+        )}
+        <Button
+          onClick={handlePays}
+          type="primary"
+          shape="round"
+          disabled={sms !== undefined ? true : false}
+        >
+          Buscar
+        </Button>
+        <div className="error_sms_id">{sms}</div>
+      </Row>
+      {success.byIdentification && (
+        <Row
+          className={
+            type === "identification"
+              ? "visible_data_payment"
+              : "hidden_data_payment"
+          }
+        >
+          <PaymentsUser
+            name={user.name}
+            lastName={user.lastName}
+            identification={user.identification}
+            payments={user.payment}
+          />
+        </Row>
       )}
-      <Button
-        onClick={handlePays}
-        type="primary"
-        shape="round"
-        disabled={sms !== undefined ? true : false}
-      >
-        Buscar
-      </Button>
-      <div className="error_sms_id">{sms}</div>
-    </Row>
+      {success.byDates && (
+        <Row
+          className={
+            type === "dates" ? "visible_data_payment" : "hidden_data_payment"
+          }
+        >
+          <p>hola</p>
+          <p>aqui debe ir un componente con el listado de pagos</p>
+        </Row>
+      )}
+    </Fragment>
   );
 };
 
