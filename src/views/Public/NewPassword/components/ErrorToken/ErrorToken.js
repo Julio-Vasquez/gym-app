e50de36df31ee1ddfe3db9444e8028813b3f503d
Julio-Vasquez <jualvalitube@gmail.com>
2021-05-07 12:31:57 -0500

    style(404): update

--- src/views/Public/NewPassword/components/ErrorToken/ErrorToken.js
+++ src/views/Public/NewPassword/components/ErrorToken/ErrorToken.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
 export const ErrorToken = () => (
-  <div id="notfound">
-  <div className="notfound">
-    <div className="notfound-404">
-      <h1>404</h1>
-      <h2>Token Invalido</h2>
+  <div id="notFound">
+    <div className="notFound">
+      <div className="notFound-404">
+        <h1>404</h1>
+        <h2>Solicitud Invalida</h2>
+      </div>
+      <a href="/">Inicio</a>
     </div>
-    <a href="/">Inicio</a>
   </div>
-</div>
 );
