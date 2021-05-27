0346e8d81e4dc0f2e9adcde2d3d723f542d4849d
Julio-Vasquez <jualvalitube@gmail.com>
2021-05-27 14:43:36 -0500

    feat(img): refactor dashboard

--- src/views/Private/Dashboard/Dashboard.js
+++ src/views/Private/Dashboard/Dashboard.js
@@ -1,11 +1,9 @@
 import React from "react";
-import dashboard from "./../../../assets/img/BannerDash.jpg";
 
 const Dashboard = () => {
   return (
     <div className="content-dashboard">
-      <div className="content-dashboard-bg">
-        <img src={dashboard} alt="Apollo Gym" />
+      <div className="content-dashboard-bg">    
       </div>
     </div>
   );
